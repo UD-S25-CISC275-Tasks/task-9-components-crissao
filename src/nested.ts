789aa3100a993713fe7179053b0878bd54ad3ae7
Crissa Owens <crissao@udel.edu>
2025-02-22 21:01:45 -0500

    sumPoints and sumPublishedPoints done

--- src/nested.ts
+++ src/nested.ts
@@ -1,3 +1,4 @@
+import Q from "q";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
 
@@ -23,8 +24,13 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    const not_empty = questions.filter(
-        (question: Question): boolean => question.body != "" || question.expected != "" || question.options.length !== 0)
+    const not_empty = questions
+        .filter(
+            (question: Question): boolean =>
+                question.body != "" ||
+                question.expected != "" ||
+                question.options.length !== 0,
+        )
         .map(
             (question: Question): Question => ({
                 ...question,
@@ -40,13 +46,13 @@ export function getNonEmptyQuestions(questions: Question[]): Question[] {
  */
 export function findQuestion(
     questions: Question[],
-    id: number
+    id: number,
 ): Question | null {
-    const found = questions.find((question: Question): boolean => question.id === id);
-    if (found !== undefined){
-        return {...found,
-            options: [...found.options]
-        };
+    const found = questions.find(
+        (question: Question): boolean => question.id === id,
+    );
+    if (found !== undefined) {
+        return { ...found, options: [...found.options] };
     }
     return null;
 }
@@ -56,8 +62,8 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    const filtered = questions.filter(
-        (question: Question): boolean => question.id !== id)
+    const filtered = questions
+        .filter((question: Question): boolean => question.id !== id)
         .map(
             (question: Question): Question => ({
                 ...question,
@@ -80,14 +86,19 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const points = questions.map((question: Question): number => question.points);
+    
+    const total = points.reduce((sum: number, point: number): number => sum + point, 0);
+    return total;
 }
 
 /***
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const published = getPublishedQuestions(questions);
+    const total = sumPoints(published);
+    return total;
 }
 
 /***
@@ -145,7 +156,7 @@ export function addNewQuestion(
     questions: Question[],
     id: number,
     name: string,
-    type: QuestionType
+    type: QuestionType,
 ): Question[] {
     return [];
 }
@@ -158,7 +169,7 @@ export function addNewQuestion(
 export function renameQuestionById(
     questions: Question[],
     targetId: number,
-    newName: string
+    newName: string,
 ): Question[] {
     return [];
 }
@@ -173,7 +184,7 @@ export function renameQuestionById(
 export function changeQuestionTypeById(
     questions: Question[],
     targetId: number,
-    newQuestionType: QuestionType
+    newQuestionType: QuestionType,
 ): Question[] {
     return [];
 }
@@ -192,7 +203,7 @@ export function editOption(
     questions: Question[],
     targetId: number,
     targetOptionIndex: number,
-    newOption: string
+    newOption: string,
 ): Question[] {
     return [];
 }
@@ -206,7 +217,7 @@ export function editOption(
 export function duplicateQuestionInArray(
     questions: Question[],
     targetId: number,
-    newId: number
+    newId: number,
 ): Question[] {
     return [];
 }
