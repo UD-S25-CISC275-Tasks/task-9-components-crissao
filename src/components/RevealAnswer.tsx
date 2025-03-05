40dbc8a74ed06551dfeb088b9677c379d02f347a
Crissa Owens <crissao@udel.edu>
2025-03-05 10:30:23 -0500

    RevealAnswer done

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,19 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisibility] = useState<boolean>(false);
+    return (
+        <span>
+            <Button
+                style={{ backgroundColor: "orange", color: "black" }}
+                onClick={() => {
+                    setVisibility(!visible);
+                }}
+            >
+                Reveal Answer
+            </Button>
+            {visible && <span> 42</span>}
+        </span>
+    );
 }
+
